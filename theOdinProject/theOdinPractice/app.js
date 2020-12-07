bc0ba89ab0d2f05c93354b9c433abcb9d2a6338c
Sergo Gabunia <sergogabunia@gmail.com>
2020-12-07 17:02:41 +0400

    added new files

---
+++ theOdinProject/theOdinPractice/app.js
@@ -0,0 +1,26 @@
+var age = 45;
+let secondName = "Gabunia";
+name = "Sergo";
+let number = 15;
+
+console.log(window.secondName);
+
+// function sayHello() {
+//   let number = 15;
+//   //   return number;
+// }
+
+// sayHello();
+// console.log(window);
+// console.log(this);
+
+var x = 1;
+
+if (x === 1) {
+  var x = 2;
+
+  console.log(x);
+  // output: 2
+}
+
+console.log(x);
